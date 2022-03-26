25d146f6f8bf7f2789aefdea064e41e5315cbbe4
amirDev00 <92927623+amirmahdimaleki@users.noreply.github.com>
2022-03-26 18:14:16 +0430

    customize

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,19 @@
 const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
+//  by using npm gulp-purgecss  --save-dev  we can use just the css rules that we used in our project and delete extra pnes
 const purgecss = require('gulp-purgecss')
 
 function buildStyles() {
   return src('sass/**/*.scss')
     .pipe(sass({ outputStyle: 'compressed' }))
+    // witch file to be used purgecss
     .pipe(purgecss({ content: ['*.html'] }))
     .pipe(dest('css'))
 }
 
+//  by using this method we can watch our css when its updated add that class
 function watchTask() {
+  //  files to be watched by watchTask
   watch(['sass/**/*.scss', '*.html'], buildStyles)
 }
 
