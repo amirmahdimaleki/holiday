86b52ba1dca13215b282e5ea409eaef4d58d4989
amirDev00 <92927623+amirmahdimaleki@users.noreply.github.com>
2022-04-02 20:40:09 +0430

    get

---
+++ JSONserver/js/details.js
@@ -0,0 +1,19 @@
+const container = document.querySelector('.details');
+
+// an obj that has the value of the "id=post.id" in browser search bar
+const id = new URLSearchParams(window.location.search).get('id');
+// if the id was 2 e.g it will be stored in this id var
+
+const renderDetails = async() =>{
+    // get a single one with the id var
+    const res = await fetch("http://localhost:3000/posts/" + id);
+    const post = await res.json();
+        //   we can log console.log(post) here to make sure it works: it should be an obj
+    const template = `
+    <h1>${post.title}</h1>
+    <p>${post.body}</p>
+    `
+    container.innerHTML = template;
+}
+
+window.addEventListener('DOMContentLoaded', ()=> renderDetails());
--- JSONserver/js/index.js
+++ JSONserver/js/index.js
@@ -4,7 +4,7 @@ const renderPosts = async ()=>{
 
       const res = await fetch(uri);
       const posts = await res.json();
-    //   we can log console.log(posts) here to make sure it works
+    //   we can log console.log(posts) here to make sure it works: it should be an array of obj
       let template = '';
       posts.forEach(post =>{
         //  += means appending
@@ -13,11 +13,11 @@ const renderPosts = async ()=>{
            <h2>${post.title}</h2>
            <p><small>${post.likes} likes</small></p>
            <p>${post.body.slice(0, 200)}</p>
-           <a href="/details.html">read more .,.,</a>
+           <a href="/details.html?id=${post.id}">read more .,.,</a>
           </div>
           `
       })
-
+//   we are trying to fetch single items by writing a key value in line 16 bt post.id
       container.innerHTML = template;
 }
 
