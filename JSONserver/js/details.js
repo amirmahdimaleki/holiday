92663f84da7e5cefce078cb7a71d1af181f8a1eb
amirDev00 <92927623+amirmahdimaleki@users.noreply.github.com>
2022-04-02 20:57:27 +0430

    bit

--- JSONserver/js/details.js
+++ JSONserver/js/details.js
@@ -1,13 +1,15 @@
-const container = document.querySelector('.details');
 
 // an obj that has the value of the "id=post.id" in browser search bar
 const id = new URLSearchParams(window.location.search).get('id');
 // if the id was 2 e.g it will be stored in this id var
 
+const container = document.querySelector('.details');
+
 const renderDetails = async() =>{
     // get a single one with the id var
-    const res = await fetch("http://localhost:3000/posts/" + id);
+    const res = await fetch('http://localhost:3000/posts/' + id);
     const post = await res.json();
+    console.log(post)
         //   we can log console.log(post) here to make sure it works: it should be an obj
     const template = `
     <h1>${post.title}</h1>
@@ -16,4 +18,4 @@ const renderDetails = async() =>{
     container.innerHTML = template;
 }
 
-window.addEventListener('DOMContentLoaded', ()=> renderDetails());
+window.addEventListener('DOMContentLoaded', () => renderDetails());
