ff5a644a4d18a15234c13e3e1ccb2da63f415fbd
amirDev00 <92927623+amirmahdimaleki@users.noreply.github.com>
2022-04-03 17:38:07 +0430

    about end

--- JSONserver/js/details.js
+++ JSONserver/js/details.js
@@ -4,18 +4,29 @@ const id = new URLSearchParams(window.location.search).get('id');
 // if the id was 2 e.g it will be stored in this id var
 
 const container = document.querySelector('.details');
+const deleteBtn = document.querySelector('.delete');
 
-const renderDetails = async() =>{
+const renderDetails = async () => {
     // get a single one with the id var
     const res = await fetch('http://localhost:3000/posts/' + id);
-    const post = await res.json();
-    console.log(post)
+    if (!res.ok) {
+        window.location.replace("/");
+      }
+      const post = await res.json();
         //   we can log console.log(post) here to make sure it works: it should be an obj
-    const template = `
-    <h1>${post.title}</h1>
-    <p>${post.body}</p>
-    `
-    container.innerHTML = template;
-}
+        const template = `
+        <h1>${post.title}</h1>
+        <p>${post.body}</p>
+      `
+    
+      container.innerHTML = template;
+    }
 
-window.addEventListener('DOMContentLoaded', () => renderDetails());
+    deleteBtn.addEventListener('click', async () => {
+        const res = await fetch('http://localhost:3000/posts/' + id, {
+          method: 'DELETE'
+        });
+        window.location.replace("/");
+      })
+      
+      window.addEventListener('DOMContentLoaded', renderDetails);
--- JSONserver/js/index.js
+++ JSONserver/js/index.js
@@ -1,28 +1,39 @@
 const container = document.querySelector('.blogs');
-const renderPosts = async ()=>{
+const searchForm = document.querySelector('.search');
+
+const renderPosts = async (term) => {
   // ?_sort lets us sort the data not by the id but e.g likes
   // &_order=desc : higher to lower
-      let uri = " http://localhost:3000/posts?_sort=likes&_order=desc";
+  let uri = 'http://localhost:3000/posts?_sort=likes&_order=desc';
+  if (term) {
+    uri += `&q=${term}`
+  }
 
-      const res = await fetch(uri);
-      const posts = await res.json();
+  const res = await fetch(uri);
+  const posts = await res.json();
     //   we can log console.log(posts) here to make sure it works: it should be an array of obj
-      let template = '';
-      posts.forEach(post =>{
-        //  += means appending
-          template += `
-          <div class="post">
-           <h2>${post.title}</h2>
-           <p><small>${post.likes} likes</small></p>
-           <p>${post.body.slice(0, 200)}...</p>
-           <a href="/details.html?id=${post.id}">read more .,.,</a>
-          </div>
-          `
-      })
+    let template = '';
+    posts.forEach(post => {
+      template += `
+        <div class="post">
+          <h2>${post.title}</h2>
+          <p><small>${post.likes} likes</small></p>
+          <p>${post.body.slice(0, 200)}...</p>
+          <a href="/details.html?id=${post.id}">Read more</a>
+        </div>
+      `
+    });
 //   we are trying to fetch single items by writing a key value in line 16 bt post.id
-      container.innerHTML = template;
+container.innerHTML = template;
 }
 
 
-window.addEventListener('DOMContentLoaded', ()=> renderPosts());
+
+// search
+searchForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  renderPosts(searchForm.term.value.trim());
+})
+
+window.addEventListener('DOMContentLoaded', () => renderPosts());
 // * runs when the dom content is loaded in the browser
